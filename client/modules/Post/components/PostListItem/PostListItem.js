0a02b98de5f872a9045639ac6b65a3778ccb4a9d
Magdalena Miszewska <mag.miszewska@gmail.com>
2018-08-08 17:30:28 +0200

    Add edit, thumbUp and thumbDown functionality

--- client/modules/Post/components/PostListItem/PostListItem.js
+++ client/modules/Post/components/PostListItem/PostListItem.js
@@ -16,6 +16,9 @@ function PostListItem(props) {
       </h3>
       <p className={styles['author-name']}><FormattedMessage id="by" /> {props.post.name}</p>
       <p className={styles['post-desc']}>{props.post.content}</p>
+      <h4 className={styles['post-votes']}><FormattedMessage id="votes: " /> {props.post.voteCount}</h4>
+      <button className={styles['btn-up']} onClick={props.thumbUp}>+</button>
+      <button className={styles['btn-down']} onClick={props.thumbDown}>-</button>
       <p className={styles['post-action']}><a href="#" onClick={props.onDelete}><FormattedMessage id="deletePost" /></a></p>
       <hr className={styles.divider} />
     </div>
@@ -29,8 +32,11 @@ PostListItem.propTypes = {
     content: PropTypes.string.isRequired,
     slug: PropTypes.string.isRequired,
     cuid: PropTypes.string.isRequired,
+    voteCount: PropTypes.number.isRequired,
   }).isRequired,
   onDelete: PropTypes.func.isRequired,
+  thumbUp: PropTypes.func.isRequired,
+  thumbDown: PropTypes.func.isRequired,
 };
 
 export default PostListItem;
